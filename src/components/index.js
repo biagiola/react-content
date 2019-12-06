f9f910d401169e12c91a49e510de2091a1528926
David <41311088+biagiola@users.noreply.github.com>
2019-12-06 12:04:56 -0300

    Add files via upload

---
+++ src/components/index.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+import './style.css';
+import Navbar from './components/Navbar02';
+import BookList from './components/Booklist';
+import ThemeContextProvider from './contexts/ThemeContext';
+
+class App extends Component {
+  render() {
+    return (
+      <div classNane="App">
+      <ThemeContextProvider>
+        <Navbar/>
+        <BookList/>
+      </ThemeContextProvider>
+      </div>
+    );
+  }
+}
+
+render(<App />, document.getElementById('root'));
