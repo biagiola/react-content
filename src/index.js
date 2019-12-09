f29be2cd7b804d5f0c417b42fb670b2ddc2d8581
David <41311088+biagiola@users.noreply.github.com>
2019-12-09 12:05:21 -0300

    multiple contexts added

--- src/index.js
+++ src/index.js
@@ -4,15 +4,28 @@ import './style.css';
 import Navbar from './components/Navbar';
 import BookList from './components/Booklist';
 import ThemeContextProvider from './contexts/ThemeContext';
+import AuthContextProvider from './contexts/AuthContext';
+import ThemeToggle from './components/ThemeToggle';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      name: 'React'
+    };
+  }
+
   render() {
     return (
-      <div classNane="App">
-      <ThemeContextProvider>
-        <Navbar/>
-        <BookList/>
-      </ThemeContextProvider>
+      <div className="App">
+        <ThemeContextProvider>
+          <AuthContextProvider>
+            <Navbar />
+            <BookList />
+            <ThemeToggle />
+          </AuthContextProvider>
+        </ThemeContextProvider>
+
       </div>
     );
   }
